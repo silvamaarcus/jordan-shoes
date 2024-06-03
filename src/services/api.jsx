ded9e7e47684a965c2181614179c45fbab231618
Marcus Silva <devsilvamarcus@gmail.com>
2024-06-03 19:34:24 -0300

    Reparo no caminho da api

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://products-json-server.vercel.app/",
+  baseURL: "https://products-json-server.vercel.app",
 });
 
 export default API;
